896668f52e05631d1f957ceafd6921cb357596d5
Marco Marassi <thtg88@users.noreply.github.com>
2021-08-29 23:03:09 +0100

    Restored default test host port to 8545

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ class TestCase extends BaseTestCase
 {
     protected Web3 $web3;
     protected string $testRinkebyHost = 'https://rinkeby.infura.io/vuethexplore';
-    protected string $testHost = 'http://127.0.0.1:7545';
+    protected string $testHost = 'http://127.0.0.1:8545';
 
     public function setUp(): void
     {
