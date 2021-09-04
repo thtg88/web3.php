fe9a0450a74fabfd63d5f8f52e36d37f6b46fb3a
Marco Marassi <thtg88@users.noreply.github.com>
2021-09-04 21:14:53 +0100

    Renamed RequestManagerTest to HttpRequestManagerTest

--- tests/Unit/HttpRequestManagerTest.php
+++ tests/Unit/HttpRequestManagerTest.php
@@ -2,15 +2,15 @@
 
 namespace Web3\Tests\Unit;
 
+use Web3\RequestManagers\HttpRequestManager;
 use Web3\Tests\TestCase;
-use Web3\RequestManagers\RequestManager;
 
-class RequestManagerTest extends TestCase
+class HttpRequestManagerTest extends TestCase
 {
     /** @test */
     public function set_host(): void
     {
-        $requestManager = new RequestManager('http://localhost:8545', 0.1);
+        $requestManager = new HttpRequestManager('http://localhost:8545', 0.1);
         $this->assertEquals($requestManager->host, 'http://localhost:8545');
         $this->assertEquals($requestManager->timeout, 0.1);
 
