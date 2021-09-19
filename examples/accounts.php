0c79676bb581004e7f152dee8baa40c8405a8c9b
Marco Marassi <thtg88@users.noreply.github.com>
2021-09-19 21:57:21 +0100

    Amended examples to not use callback

--- examples/accounts.php
+++ examples/accounts.php
@@ -5,22 +5,24 @@ require('./exampleBase.php');
 $eth = $web3->eth;
 
 echo 'Eth Get Account and Balance' . PHP_EOL;
-$eth->accounts(function ($err, $accounts) use ($eth) {
+
+[$err, $accounts] = $eth->accounts();
+
+if ($err !== null) {
+    echo 'Error: ' . $err->getMessage();
+
+    return;
+}
+foreach ($accounts as $account) {
+    echo 'Account: ' . $account . PHP_EOL;
+
+    [$err, $balance] = $eth->getBalance($account);
+
     if ($err !== null) {
         echo 'Error: ' . $err->getMessage();
 
         return;
     }
-    foreach ($accounts as $account) {
-        echo 'Account: ' . $account . PHP_EOL;
 
-        $eth->getBalance($account, function ($err, $balance) {
-            if ($err !== null) {
-                echo 'Error: ' . $err->getMessage();
-
-                return;
-            }
-            echo 'Balance: ' . $balance . PHP_EOL;
-        });
-    }
-});
+    echo 'Balance: ' . $balance . PHP_EOL;
+}
